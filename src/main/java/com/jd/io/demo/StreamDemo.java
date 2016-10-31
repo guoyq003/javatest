ebc28a9b6c5479cbde0a8e7835181598b4a68906
guoyq <65698173@qq.com>
2016-10-31 22:07:12 +0800

    change pom.xml

--- src/main/java/com/jd/io/demo/StreamDemo.java
+++ src/main/java/com/jd/io/demo/StreamDemo.java
@@ -1,7 +1,25 @@
 package com.jd.io.demo;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * Created by guoyq on 2016/10/31.
  */
 public class StreamDemo {
+    public void readFileTest(String fileName){
+        FileInputStream fis=null;
+        try {
+            fis=new FileInputStream(fileName);
+            byte[] arr=new byte[50];
+            int len=fis.read(arr);
+            String data=new String(arr,0,len);
+            System.out.println(data);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
