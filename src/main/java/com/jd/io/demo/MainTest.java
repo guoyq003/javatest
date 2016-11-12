be06853f23a5474ba0dea5f767ca3f0b0304c2e2
guoyq <65698173@qq.com>
2016-11-12 20:33:46 +0800

    change pom.xml

---
+++ src/main/java/com/jd/io/demo/MainTest.java
@@ -0,0 +1,12 @@
+package com.jd.io.demo;
+
+/**
+ * Created by guoyq on 2016/10/31.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        String fileName="D:\\javatest\\test.txt";
+        StreamDemo streamDemo=new StreamDemo();
+        streamDemo.readFileTest(fileName);
+    }
+}
