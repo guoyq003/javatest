7bf68b538e2aca64db71f3b827b837e6c2c917ef
guoyiqing1 <guoyiqing1@jd.com>
2016-11-25 14:11:16 +0800

    add

--- src/main/java/com/jd/datedemo/datedemo.java
+++ src/main/java/com/jd/datedemo/datedemo.java
@@ -1,4 +1,31 @@
 package com.jd.datedemo;
 
+import javafx.scene.chart.PieChart;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class datedemo {
+    public static void stringToDate(String dateStr) throws ParseException {
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyyMMddHHmmss");
+        Date date=simpleDateFormat.parse(dateStr);
+        simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String formattedTime = simpleDateFormat.format(date);
+        System.out.println("---->将" + dateStr + "解析为:" + formattedTime);
+    }
+    public static String refFormatNowDate(String time) {
+        Date nowTime = new Date(Long.valueOf(time));
+        SimpleDateFormat sdFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String retStrFormatNowDate = sdFormatter.format(nowTime);
+        return retStrFormatNowDate;
+    }
+    public static void main(String[] args) throws ParseException {
+        String dateStr="20161227085009";
+        stringToDate(dateStr);
+        String time=String.valueOf(System.currentTimeMillis());
+        System.out.println(refFormatNowDate(time));
+
+    }
+
 }
