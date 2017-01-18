84a7c56ebbc6c9fe201a74af5882a4d3512d1dad
guoyiqing1 <guoyiqing1@jd.com>
2017-01-18 10:05:13 +0800

    add

--- src/main/java/com/jd/map/demo/MapTest.java
+++ src/main/java/com/jd/map/demo/MapTest.java
@@ -1,6 +1,6 @@
 package com.jd.map.demo;
 
-import com.jd.spring.demo.Car;
+//import com.jd.spring.demo.Car;
 import org.apache.commons.io.FileUtils;
 
 import javax.swing.plaf.FontUIResource;
@@ -11,62 +11,63 @@ import java.util.*;
  * Created by guoyiqing1 on 2016/5/27.
  */
 public class MapTest {
-    public static void main(String[] args) {
-        Map<Integer,String> map=new HashMap<Integer, String>();
-        map.put(1,"zhangsan");
-        map.put(2,"lisi");
-        map.put(3,"guanyu");
-        //遍历方式1
-        Set<Integer> keySet=map.keySet();
-        for (Iterator<Integer> iterator=keySet.iterator();iterator.hasNext();){
-            Integer key=iterator.next();
-            String value=map.get(key);
-            System.out.println(key+"="+value);
-        }
-        System.out.println("-------------------------------");
-        //遍历方式2
-        Set entrySet=map.entrySet();
-        for (Iterator iterator=entrySet.iterator();iterator.hasNext();){
-            Map.Entry entry= (Map.Entry) iterator.next();
-            Integer key= (Integer) entry.getKey();
-            String value= (String) entry.getValue();
+//    public static void main(String[] args) {
+//        Map<Integer,String> map=new HashMap<Integer, String>();
+//        map.put(1,"zhangsan");
+//        map.put(2,"lisi");
+//        map.put(3,"guanyu");
+//        //遍历方式1
+//        Set<Integer> keySet=map.keySet();
+//        for (Iterator<Integer> iterator=keySet.iterator();iterator.hasNext();){
+//            Integer key=iterator.next();
+//            String value=map.get(key);
 //            System.out.println(key+"="+value);
-            System.out.println(entry.getKey()+"="+entry.getValue());
-        }
-        System.out.println("---------------3----------------");
-        //遍历方法3
-        for (Map.Entry entry:map.entrySet()){
-            System.out.println(entry.getKey()+"="+entry.getValue());
-        }
-        System.out.println("---------------4----------------");
-        Map<String,Car> carMap=new HashMap<String, Car>();
-        carMap.put("BMW",new Car("BMW","red",230));
-        carMap.put("AODI",new Car("AODI","yellow",200));
-        Set<String> set=carMap.keySet();
-        Iterator<String> iterator1=set.iterator();
-        while (iterator1.hasNext()){
-            String key=iterator1.next();
-            Object value=carMap.get(key);
-            System.out.println(key+"="+value);
-        }
-        System.out.println("---------------5----------------");
-        Set carSet=carMap.keySet();
-        for (Iterator iterator=carSet.iterator();iterator.hasNext();){
-            String key= (String) iterator.next();
-            Car value=carMap.get(key);
-            System.out.println(key+"="+value);
-        }
-        System.out.println("---------------6----------------");
-        Set entries=carMap.entrySet();
-        for (Iterator iterator=entries.iterator();iterator.hasNext();){
-            Map.Entry entry= (Map.Entry) iterator.next();
-            String key= (String) entry.getKey();
-            Car value= (Car) entry.getValue();
-            System.out.println(key+"="+value);
-        }
-        System.out.println("---------------7----------------");
-        for (Map.Entry entry:carMap.entrySet()){
-            System.out.println(entry.getKey()+"="+entry.getValue());
-        }
-    }
+//        }
+//        System.out.println("-------------------------------");
+//        //遍历方式2
+//        Set entrySet=map.entrySet();
+//        for (Iterator iterator=entrySet.iterator();iterator.hasNext();){
+//            Map.Entry entry= (Map.Entry) iterator.next();
+//            Integer key= (Integer) entry.getKey();
+//            String value= (String) entry.getValue();
+////            System.out.println(key+"="+value);
+//            System.out.println(entry.getKey()+"="+entry.getValue());
+//        }
+//        System.out.println("---------------3----------------");
+//        //遍历方法3
+//        for (Map.Entry entry:map.entrySet()){
+//            System.out.println(entry.getKey()+"="+entry.getValue());
+//        }
+//        System.out.println("---------------4----------------");
+//        Map<String,Car> carMap=new HashMap<String, Car>();
+//        carMap.put("BMW",new Car("BMW","red",230));
+//        carMap.put("AODI",new Car("AODI","yellow",200));
+//        Set<String> set=carMap.keySet();
+//        Iterator<String> iterator1=set.iterator();
+//        while (iterator1.hasNext()){
+//            String key=iterator1.next();
+//            Object value=carMap.get(key);
+//            System.out.println(key+"="+value);
+//        }
+//        System.out.println("---------------5----------------");
+//        Set carSet=carMap.keySet();
+//        for (Iterator iterator=carSet.iterator();iterator.hasNext();){
+//            String key= (String) iterator.next();
+//            Car value=carMap.get(key);
+//            System.out.println(key+"="+value);
+//        }
+//        System.out.println("---------------6----------------");
+//        Set entries=carMap.entrySet();
+//        for (Iterator iterator=entries.iterator();iterator.hasNext();){
+//            Map.Entry entry= (Map.Entry) iterator.next();
+//            String key= (String) entry.getKey();
+//            Car value= (Car) entry.getValue();
+//            System.out.println(key+"="+value);
+//        }
+//        System.out.println("---------------7----------------");
+//        for (Map.Entry entry:carMap.entrySet()){
+//            System.out.println(entry.getKey()+"="+entry.getValue());
+//        }
+//    }
+//
 }
