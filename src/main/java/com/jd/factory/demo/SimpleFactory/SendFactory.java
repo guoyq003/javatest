dbfcd1b3ed109308c58bb803b6d62d30e2858c2a
guoyiqing1 <guoyiqing1@jd.com>
2017-01-22 15:03:35 +0800

    add

--- src/main/java/com/jd/factory/demo/SimpleFactory/SendFactory.java
+++ src/main/java/com/jd/factory/demo/SimpleFactory/SendFactory.java
@@ -1,14 +1,29 @@
 package com.jd.factory.demo.SimpleFactory;
 
 public class SendFactory {
-    public Sender produce(String type){
-        if ("mail".equals(type)){
-            return new MailSender();
-        }else if ("sms".equals(type)){
-            return new SmsSender();
-        }else {
-            System.out.println("请输入正确的类型！");
-            return null;
-        }
+//    //普通工厂方法
+//    public Sender produce(String type){
+//        if ("mail".equals(type)){
+//            return new MailSender();
+//        }else if ("sms".equals(type)){
+//            return new SmsSender();
+//        }else {
+//            System.out.println("请输入正确的类型！");
+//            return null;
+//        }
+//    }
+    //多个工厂方法
+//    public  Sender produceMail(){
+//        return new MailSender();
+//    }
+//    public Sender produceSma(){
+//        return new SmsSender();
+//    }
+    //静态工厂方法模式
+    public static Sender produceMail(){
+        return new MailSender();
+    }
+    public static Sender produceSma(){
+        return new SmsSender();
     }
 }
