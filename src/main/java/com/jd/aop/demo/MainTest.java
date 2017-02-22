367fc1563bbb193bd4cb8ad516ddf8bc3455b45b
guoyiqing1 <guoyiqing1@jd.com>
2017-02-22 15:46:45 +0800

    add

--- src/main/java/com/jd/aop/demo/MainTest.java
+++ src/main/java/com/jd/aop/demo/MainTest.java
@@ -5,17 +5,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainTest {
     public static void main(String[] args) {
-//        //使用aop切点
-//        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
-//        AirthmeticCalculator airthmeticCalculator=context.getBean(AirthmeticCalculator.class);
-//        int result=airthmeticCalculator.add(3,7);
-//        System.out.println(result);
-//        int result1=airthmeticCalculator.add(10,2);
-//        System.out.println(result1);
-        AirthmeticCalculator target=new AirthmeticCalculatorImpl();
-        AirthmeticCalculator proxy=new AirthmeticCalculatorLoggingProxy(target).getLoggingProxy();
-        int result=proxy.add(2,8);
+        //使用aop切点
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+        AirthmeticCalculator airthmeticCalculator=context.getBean(AirthmeticCalculator.class);
+        int result=airthmeticCalculator.add(3,7);
         System.out.println(result);
-        
+        int result1=airthmeticCalculator.add(10,2);
+        System.out.println(result1);
+//        AirthmeticCalculator target=new AirthmeticCalculatorImpl();
+//        AirthmeticCalculator proxy=new AirthmeticCalculatorLoggingProxy(target).getLoggingProxy();
+//        int result=proxy.add(2,8);
+//        System.out.println(result);
+
     }
 }
