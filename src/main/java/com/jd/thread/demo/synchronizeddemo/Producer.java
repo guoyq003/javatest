c3466c34f7ca459e24eb08f5a3cff85941269162
guoyiqing1 <guoyiqing1@jd.com>
2017-02-28 16:16:21 +0800

    add ThreadDemo

---
+++ src/main/java/com/jd/thread/demo/synchronizeddemo/Producer.java
@@ -0,0 +1,31 @@
+package com.jd.thread.demo.synchronizeddemo;
+
+public class Producer extends Thread {
+    private int num;
+    private Storage storage;
+    public Producer(Storage storage){
+        this.storage=storage;
+    }
+    public void run(){
+        produce(num);
+    }
+    public void produce(int num){
+        storage.produce(num);
+    }
+
+    public Storage getStorage() {
+        return storage;
+    }
+
+    public void setStorage(Storage storage) {
+        this.storage = storage;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
