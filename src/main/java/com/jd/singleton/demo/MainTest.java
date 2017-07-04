717eca095eeb167248b6433602bfc8c99a7f7dbc
guoyiqing1 <guoyiqing1@jd.com>
2017-07-04 17:54:20 +0800

    add ThreadDemo

--- src/main/java/com/jd/singleton/demo/MainTest.java
+++ src/main/java/com/jd/singleton/demo/MainTest.java
@@ -2,6 +2,7 @@ package com.jd.singleton.demo;
 
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println(Singleton01.getInstance().add(3,2));
+//        System.out.println(Singleton01.getInstance().add(3,2));
+        System.out.println(Singleton02.getIstance().div(4,2));
     }
 }
